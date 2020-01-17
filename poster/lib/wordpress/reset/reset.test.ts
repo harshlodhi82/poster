5dd4344b7d3dd573bdf9e0b500523a6e2f29187d
Dave <davedmarketing@gmail.com>
2020-01-18 03:40:21 +0700

    a

--- poster/lib/wordpress/reset/reset.test.ts
+++ poster/lib/wordpress/reset/reset.test.ts
@@ -1,9 +1,11 @@
 import reset from './index'
 import credentials from '../fixtures/credentials'
+import createPost from '../createPost'
 
 test('reset', async () => {
   const wordpress = {
     reset,
+    createPost,
     ...credentials
   }
   Object.freeze(wordpress)
