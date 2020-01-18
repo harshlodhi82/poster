6b4b6bada3480ae5734007c9ad60dd0ab530d261
Harsh Lodhi <harshlodhi82@gmail.com>
2020-01-18 23:10:01 +0530

    Working on reset

--- poster/lib/wordpress/reset/index.ts
+++ poster/lib/wordpress/reset/index.ts
@@ -1,5 +1,6 @@
 import log from 'lib/utils/logger'
 import WPAPI from 'wpapi'
+import c
 
 interface Reset {
   (): Promise<void>
@@ -9,6 +10,31 @@ interface Reset {
 const reset: Reset = async function () {
   /* reset should delete all posts, categories, and medias */
   log.info(this.url, this.username, this.password)
+  log.info('Running Here!')
+
+  // let data = {title, content, imageUrls, categories, status}
+  const URL = this.url + 'wp-json'
+  const wp = new WPAPI({
+    endpoint: URL,
+    username: this.username,
+    password: this.password,
+    auth: true
+  })
+  try {
+    let posts = await wp.posts()
+    // log.info('posts>>>>', posts)
+    for (let index = 0; index < posts.length; index++) {
+      const post = posts[index]
+      log.info('Deleteing >>>>', post.id)
+      await wp.posts().id(post.id).delete()
+    }
+    // let data =
+    // let posts2 = await wp.posts()
+    log.info('Deleted >>>>')
+  }
+  catch (error) {
+    log.info('error>>>>', error)
+  }
 }
 
 export default reset
